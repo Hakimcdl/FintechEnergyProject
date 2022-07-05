4c15b4402896e68726f95f6484dd1a985575213e
Théo Attali <theoattali@gmail.com>
2022-07-05 18:14:42 -0300

    remove console.log

--- public/asset/js/rowlArticle.js
+++ public/asset/js/rowlArticle.js
@@ -1,14 +1,9 @@
 const deroulant = document.querySelectorAll('.deroulant')
 const eachArticle = document.querySelector('.eachArticle')
 
-
 deroulant.forEach(element =>{
-    console.log('ok', element);
     element.addEventListener('click',()=> {
-        console.log(element);
         element.parentElement.classList.toggle('active')
         element.classList.toggle('rotate')
     })
 });
-
-//
